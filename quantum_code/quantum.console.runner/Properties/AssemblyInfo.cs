ce6cb4cf3f4a46ed279a9dfb3f4ecf8346b9cf2e
Bugrahan Demir <bugrahandemir@gmail.com>
2023-12-15 18:03:42 +0300

    Initial commit

---
+++ quantum_code/quantum.console.runner/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("quantum.console.runner")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("quantum.console.runner")]
+[assembly: AssemblyCopyright("Copyright © Exit Games 2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("b2c620e4-13f8-4ecd-a7f0-88724216e90c")]
+[assembly: AssemblyVersion("2.1.7")]
+[assembly: AssemblyFileVersion("2.1.7")]
+[assembly: AssemblyInformationalVersion("2.1.7 Stable 1164 2.1/develop (53b66494c)")]
