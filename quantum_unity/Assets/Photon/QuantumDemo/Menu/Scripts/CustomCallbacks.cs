f5929ff78e606904bebed50658d950f94e95c97d
Bugrahan Demir <bugrahandemir@gmail.com>
2023-12-15 22:54:35 +0300

    Add real time player spawn on menu

--- quantum_unity/Assets/Photon/QuantumDemo/Menu/Scripts/CustomCallbacks.cs
+++ quantum_unity/Assets/Photon/QuantumDemo/Menu/Scripts/CustomCallbacks.cs
@@ -1,20 +1,27 @@
-﻿using UnityEngine;
+﻿using Quantum;
+using UnityEngine;
 
-public class CustomCallbacks : QuantumCallbacks {
+public class CustomCallbacks : QuantumCallbacks 
+{
+	public RuntimePlayer PlayerData;
 
-  public override void OnGameStart(Quantum.QuantumGame game) {
-    // paused on Start means waiting for Snapshot
-    if (game.Session.IsPaused) return;
+	public override void OnGameStart(Quantum.QuantumGame game)
+	{
+		// IsPaused is true when a player late joins or reconnects to a room.
+		// This prevents the spawning of another player object when reconnecting.
+		if (game.Session.IsPaused) return;
 
-    foreach (var lp in game.GetLocalPlayers()) {
-      Debug.Log("CustomCallbacks - sending player: " + lp);
-      game.SendPlayerData(lp, new Quantum.RuntimePlayer { });
-    }
-  }
+		foreach (var localPlayer in game.GetLocalPlayers())
+		{
+			Debug.Log("CustomCallbacks - sending player: " + localPlayer);
+			game.SendPlayerData(localPlayer, PlayerData);
+		}
+	}
 
-  public override void OnGameResync(Quantum.QuantumGame game)
-  {
-    Debug.Log("Detected Resync. Verified tick: " + game.Frames.Verified.Number);
-  }
+	public override void OnGameResync(Quantum.QuantumGame game)
+	{
+		// OnGameResync is called when a player reconnects.
+		Debug.Log("Detected Resync. Verified tick: " + game.Frames.Verified.Number);
+	}
 }
 
